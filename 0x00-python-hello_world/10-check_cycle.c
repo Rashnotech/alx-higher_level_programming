c97bd76491825125e136bbfaf1cc97d15010e6cf
Rashnotech <rashnotech@gmail.com>
2023-06-05 13:01:14 -0700

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,4 +6,15 @@
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *slow, *fast;
+
+	slow = fast = list;
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
 }
