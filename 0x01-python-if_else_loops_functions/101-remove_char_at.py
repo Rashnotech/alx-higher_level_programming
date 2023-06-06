55ebb3c22c9867e336ffa9d31872b6fff9b4ea3b
Rashnotech <rashnotech@gmail.com>
2023-06-06 16:17:29 -0700

    Remove characters at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -5,9 +5,3 @@ def remove_char_at(str, n):
         if x != n:
             new += str[x]
     return new
-
-print(remove_char_at("Best School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
