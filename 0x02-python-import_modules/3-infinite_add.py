ccfc7aa95c947d8511ddaf35d0233c59ebc39a6b
Rashnotech <rashnotech@gmail.com>
2023-06-08 04:21:56 -0700

    Sum all arguments in the command line

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,10 @@
 
 import sys
 
-arg = sys.argv[1:]
-args = len(arguments)
-for i in range(argument):
-    print('{}: {}'.format(i + 1, arguments[i]))
+args = sys.argv[1:]
+arg = len(args)
+ret = 0
+for i in range(arg):
+    ret += int(args[i])
+if __name__ == '__main__':
+    print('{}'.format(ret))
