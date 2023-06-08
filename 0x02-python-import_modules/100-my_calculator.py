b17f603d6670a48892410c5b7a8a1a5f11d9e680
Rashnotech <rashnotech@gmail.com>
2023-06-08 10:18:26 -0700

    Simple calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,10 +3,10 @@
 import sys
 from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
-    args = sys.argv[:4]
+    args = sys.argv[1:]
     arg = len(args)
     if arg != 3:
-        print('{} <a> <operator> <b>'.format(args[0]))
+        print('{} <a> <operator> <b>'.format(sys.argv[0]))
         sys.exit(1)
     a = int(args[1])
     ops = args[2]
