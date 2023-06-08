23ff01eadb113aefefad28f208baf441a9dcd344
Rashnotech <rashnotech@gmail.com>
2023-06-08 05:51:47 -0700

    Prints all the name defined by the compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
+import hidden_4
 if __name__ == '__main__':
-    module = dir("hidden_4")
+    module = dir(hidden_4)
     for name in module:
         if name[:2] != '__':
             print('{}'.format(name))
