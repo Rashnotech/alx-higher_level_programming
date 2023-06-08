d579612138c6bde2b8782c576b70c17f1f91471e
Rashnotech <rashnotech@gmail.com>
2023-06-08 04:09:12 -0700

    Print argument in python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+arguments = sys.argv[1:]
+argument = len(arguments)
+if argument == 0:
+    print('{} arguments.'.format("0"))
+elif argument == 1:
+    print('{} argument:'.format(argument))
+else:
+    print('{} arguments:'.format(argument))
+for i in range(argument):
+    print('{}: {}'.format(i + 1, arguments[i]))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import sys
+
+arg = sys.argv[1:]
+args = len(arguments)
+for i in range(argument):
+    print('{}: {}'.format(i + 1, arguments[i]))
