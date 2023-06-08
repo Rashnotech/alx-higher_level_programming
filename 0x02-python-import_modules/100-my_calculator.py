919d4cce7eb2172e4a6f1c4cd99b67fed6b9e293
Rashnotech <rashnotech@gmail.com>
2023-06-08 10:02:06 -0700

    Simple calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,7 +16,6 @@ if __name__ == '__main__':
     elif ops == "-":
         print('{} {} {} {} {}'.format(a, "-", b, "=", sub(a, b)))
     elif ops == "*":
-        print('{}{}'.format(a, b))
         print('{} {} {} {} {}'.format(a, "*", b, "=", mul(a, b)))
     elif ops == "/":
         print('{} {} {} {} {:.0f}'.format(a, "/", b, "=", div(a, b)))
