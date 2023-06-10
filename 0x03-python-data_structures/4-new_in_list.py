7f25f373426d6be2ea90490d576dae5246b06d8d
Rashnotech <rashnotech@gmail.com>
2023-06-10 21:28:31 +0000

    Data Structure on Tuples and List

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    size = len(my_list)
-    if idx < 0 or idx > size or size == 0:
+    if idx < 0 or idx > size:
         return (my_list)
     else:
         list = my_list.copy()
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,6 @@
 def no_c(my_string):
     element = ""
     for i in range(len(my_string)):
-        if my_string[i] in ["c", "C"]:
-            element += " "
-        else:
+        if my_string[i] not in ["c", "C"]:
             element += my_string[i]
     return element
