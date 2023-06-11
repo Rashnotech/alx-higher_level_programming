6b1baabf8a75ab4f97229f61c540865ed07ed183
Rashnotech <rashnotech@gmail.com>
2023-06-11 00:11:02 +0000

    Check if singly linked list is palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * is_palindrom - Check if a singly linked list is a palindrome
+ * @head: a struct node pointer
+ * Return: an integer 0 if it's not a palindrome otherwise 1
+ */
+int is_palindrome(listint_t **head)
+{
+	return (0);
+}
