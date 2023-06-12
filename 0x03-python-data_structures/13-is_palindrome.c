d4973b57ef1b3df456cb154b82d01827c4036f9b
Rashnotech <rashnotech@gmail.com>
2023-06-12 22:02:47 +0000

    Check if the linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,5 +6,27 @@
  */
 int is_palindrome(listint_t **head)
 {
-	return (0);
+	listint_t *fast, *slow, *next, *prev;
+
+	slow = fast = *head;
+	next = prev = NULL;
+
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		next = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = next;
+	}
+	if (fast)
+		slow = slow->next;
+	while (slow && prev)
+	{
+		if (s:low->n != prev->n)
+			return (0);
+		slow = slow->next;
+		prev = prev->next;
+	}
+	return (1);
 }
