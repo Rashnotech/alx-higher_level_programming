0fc1fdcc785ba0d5c8a81a6acc0cebd2df55ee45
Rashnotech <rashnotech@gmail.com>
2023-06-14 21:38:33 +0000

    Fix search and replace error checks

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = list(my_list)
-    size = len(my_list)
-    if size != 0:
-        for i in my_list:
-            if new_list[i] == search:
-                new_list[i] = replace
-    return new_list
+    return [replace if x == search else x for x in my_list]
