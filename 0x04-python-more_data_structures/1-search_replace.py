175a31c2a7ed5eb68aa26ed5d4b92276e87475a6
Rashnotech <rashnotech@gmail.com>
2023-06-14 15:44:43 +0000

    Search and replace error fixes

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     lists = my_list.copy()
-    if search == 0 or search >= len(lists):
-        return lists
-    lists[search - 1] = replace
+    if search != 0 or search < len(lists):
+        lists[search - 1] = replace
     return lists
-
