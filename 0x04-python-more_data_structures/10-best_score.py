0ffdda6c63709e0edfb4e9a99b9dd093a5b9391b
Rashnotech <rashnotech@gmail.com>
2023-06-14 16:14:42 +0000

    Find the key of the best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        best = max(a_dictionary.items(), key=a_dictionary.get)
+        best = max(a_dictionary, key=a_dictionary.get)
         return best
     else:
         return None
