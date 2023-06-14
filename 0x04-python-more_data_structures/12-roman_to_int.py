1040f1f8454b84bf663c0ff1c4c24f89affdf735
Rashnotech <rashnotech@gmail.com>
2023-06-14 17:12:47 +0000

    convert roman numeral to an integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    digit = 0
+    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+              'M': 1000, 'D': 500, 'C': 100, 'IX': 9}
+    if len(roman_string) == 0 or roman_string.isalpha():
+        upper = roman_string.upper()
+        for num in upper:
+            if num in r_dict.keys():
+                digit += r_dict[num]
+    return digit
--- 0x04-python-more_data_structures/python_playground.py
+++ 0x04-python-more_data_structures/python_playground.py
@@ -7,21 +7,6 @@ def square_matrix_simple(matrix=[]):
         new_matrix.append(lists)
     return new_matrix
 
-def multiply_list_map(my_list=[], number=0):
-    lists = my_list.copy()
-    new_list[] = lists.map(element => element * number)
-    return new_list
-
-def roman_to_int(roman_string):
-    numeric, roman_dict = 0, {'I' : 1, 'V' : 5, 'X': 10, 'L' : 50,
-              'M' : 1000, 'D' : 500, 'C' : 100, 'IX' : 9}
-    size = len(roman_string)
-    if size != 0 and roman_string.alpha():
-        for r_num in roman_string:
-            if r_num.upper() in roman_dict.keys():
-                numeric += roman_dict.values()
-    return numeric
-
 def weight_average(my_list=[]):
     avg, div = 0, 0
     if len(my_list) != 0:
