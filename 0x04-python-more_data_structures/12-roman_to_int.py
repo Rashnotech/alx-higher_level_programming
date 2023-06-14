d89dc7113cc4fd45069d98ec8756998113ace094
Rashnotech <rashnotech@gmail.com>
2023-06-14 17:31:26 +0000

    convert roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     digit = 0
-    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-              'M': 1000, 'D': 500, 'C': 100, 'IX': 9}
+    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'XL': 40
+              'M': 1000, 'D': 500, 'C': 100, 'IX': 9, 'XC': 90
+              'CM': 900, 'CL': 150, 'CD': 400}
     if len(roman_string) == 0 or roman_string.isalpha():
         upper = roman_string.upper()
         for num in upper:
