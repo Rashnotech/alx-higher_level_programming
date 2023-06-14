d9b7cacb155a768dcebeca370f5f451a713f69b6
Rashnotech <rashnotech@gmail.com>
2023-06-14 22:54:55 +0000

    Handled roman numbers such as 9, 40 and others

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     digit = 0
-    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-              'M': 1000, 'D': 500, 'C': 100, 'IX': 9}
-    if len(roman_string) == 0 or roman_string.isalpha():
-        upper = roman_string.upper()
-        for num in upper:
-            if num in r_dict.keys():
-                digit += r_dict[num]
+    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'XL': 40,
+              'M': 1000, 'D': 500, 'C': 100, 'IX': 9, 'CM': 900,
+              'CD': 400, 'LC': 150, 'XC': 90}
+    if len(roman_string) != 0 or roman_string.isalpha():
+        r_man = roman_string.upper()
+        length = len(r_man)
+        for i in range(length - 1):
+            char = r_man[i]
+            if char + r_man[i + 1] in r_dict.keys():
+                digit += r_dict[char + r_man[i + 1]]
+                i +=1
+            elif char in r_dict.keys():
+                digit += r_dict[char]
+        if length != 2 and r_man[-1] in r_dict.keys():
+            digit += r_dict[r_man[-1]
     return digit
