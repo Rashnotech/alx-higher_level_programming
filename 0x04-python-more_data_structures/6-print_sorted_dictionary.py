9df312197696987c814f14dc7623f11581e45156
Rashnotech <rashnotech@gmail.com>
2023-06-14 15:52:36 +0000

    Print sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     diction_copy = a_dictionary.copy()
-    for key, values in sorted(diction_copy):
+    for key, values in sorted(diction_copy.items()):
         print('{}: {}'.format(key, values))
