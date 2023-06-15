c8e56cfa1f50e7043de0e169dca492ce25d1c90a
Rashnotech <rashnotech@gmail.com>
2023-06-15 09:25:55 +0000

    Check if the string is an instance of str

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <Python.h>
+/**
+ * print_python_list - Print python list in header file
+ * @p: a pyobject
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t 
+}
--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -2,7 +2,7 @@
 roman_to_int = __import__('12-roman_to_int').roman_to_int
 roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-roman_number = "X"
+roman_number = "[0d]'"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "VII"
