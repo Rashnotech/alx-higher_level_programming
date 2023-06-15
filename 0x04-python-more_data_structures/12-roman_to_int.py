0b3949a827df9b4e01d44ee8670a41222841cef2
Rashnotech <rashnotech@gmail.com>
2023-06-15 00:31:43 +0000

    Fixed comparison of current and next value in list

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-def alpha(s):
-    for c in s:
-        if not c.isalpha():
-            return 1
-    return 0
-
 def roman_to_int(roman_string):
-    digit = 0
-    if alpha(roman_string) == 1 or roman_string is None:
+    digit = []
+    if not roman_string or roman_string.isalpha():
         return 0
-    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'XL': 40, 'M': 1000,
-              'D': 500, 'C': 100, 'IX': 9, 'CM': 900,
-              'CD': 400, 'LC': 150, 'XC': 90}
-    if len(roman_string) != 0 or alpha(roman_string) == 0:
-        r_man = roman_string.upper()
-        length = len(r_man)
-        for i in range(length - 1):
-            char = r_man[i]
-            if char + r_man[i + 1] in r_dict.keys():
-                digit += r_dict[char + r_man[i + 1]]
-                i += 1
-            elif char in r_dict.keys():
-                digit += r_dict[char]
-        if length != 2 and r_man[-1] in r_dict.keys():
-            digit += r_dict[r_man[-1]]
-            return digit
+    diction = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'M': 1000,
+               'D': 500, 'C': 100}
+    upper = roman_string.upper()
+    for r_num in upper:
+        if r_num in diction.keys():
+            digit.append(diction[r_num])
+    num = 0
+    size = len(digit)
+    for i in range(size):
+        if i < size - 1 and digit[i] < digit[i+1]:
+            num -= digit[i]
+        else:
+            num += digit[i]
+    return num
