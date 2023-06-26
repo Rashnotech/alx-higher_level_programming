22d14aded8faa6d4b546c3626785c2eb3c9833a7
Rashnotech <rashnotech@gmail.com>
2023-06-26 17:58:11 +0000

    Remove semi-colon from function

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         for index in range(x):
             print('{}'.format(my_list[index]), end="")
-            counter += 1;
+            counter += 1
     except IndexError:
-        print();
+        print()
     else:
         print();
-    return counter;
+    return counter
