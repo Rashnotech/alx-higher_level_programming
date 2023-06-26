32e1b2edb3ee2ef57a62e32aa8c203f26d644201
Rashnotech <rashnotech@gmail.com>
2023-06-26 17:59:17 +0000

    Removed semi colon from file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    counter = 0;
+    counter = 0
     try:
         for index in range(x):
             print('{}'.format(my_list[index]), end="")
