f605147cdfcf4192811effe6b9831c41fa08d294
Rashnotech <rashnotech@gmail.com>
2023-06-26 23:56:43 +0000

    Modified the pointer

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     result = 0
     try:
-        result = fct(args[0], args[1])
+        result = fct(*args)
     except Exception as err:
         sys.stderr.write(f"Exception: {str(err)}\n")
         return None
