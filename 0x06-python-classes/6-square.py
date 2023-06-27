3a17cd8ecd71051bb2a4f21f9296bee413ee66b1
Rashnotech <rashnotech@gmail.com>
2023-06-27 22:44:28 +0000

    Error not a tuple

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,7 +32,7 @@ class Square:
         """ a method that set position attribute """
         if len(value) != 2 or not all(isinstance(num, int) for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif not isinstance(value, tuple) and int(value[0]) < 0:
+        elif not isinstance(value, tuple) or int(value[0]) < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif int(value[1]) < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
