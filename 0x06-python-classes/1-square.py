6a107869ed2fb4cefb75e41bd28e978f6cf10896
Rashnotech <rashnotech@gmail.com>
2023-06-27 17:46:00 +0000

    Documentation Error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+    Square: a class initialization
+"""
+
+
 class Square:
     """A Class Declaration with a constructor Function"""
+
     def __init__(self, size):
+        """ Private attribute initialization """
         self.__size = size
