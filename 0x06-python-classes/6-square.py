30fec636e128cfedfe72ffc81d1d18209194ef7c
Rashnotech <rashnotech@gmail.com>
2023-06-27 22:35:30 +0000

    Check the negative value

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,7 +32,7 @@ class Square:
         """ a method that set position attribute """
         if len(value) != 2 or not all(isinstance(num, int) for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif isinstance(value, tuple) and value[0] < 0 or value[1] < 0:
+        elif isinstance(value, tuple) or int(value[0]) < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
