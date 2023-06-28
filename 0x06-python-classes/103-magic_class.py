dfca38de9589508b655492e25ca412f5b4435b58
Rashnotech <rashnotech@gmail.com>
2023-06-28 09:27:16 +0000

    Fixed: Error on Exception Type

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,6 +2,7 @@
 import math
 """
     MagicClass: a magic class that can find the area and circumference
+
 """
 
 
@@ -16,9 +17,12 @@ class MagicClass:
             a circle
     """
     def __init__(self, radius=0):
-        if not (type(radius) == int or type(radius) == float):
+        if type(radius) is not int:
             raise TypeError('radius must be a number')
-        self._MagicClass__radius = radius
+        elif type(radius) is not float:
+            raise TypeError('radius must be a number')
+        else:
+            self._MagicClass__radius = radius
 
     def area(self):
         """ A method that find the area of a circle """
@@ -27,3 +31,6 @@ class MagicClass:
     def circumference(self):
         """ A method that find the circumference of a circle """
         return self._MagicClass__radius * 2 * math.pi
+
+circle = MagicClass("3")
+print(circle.area())
