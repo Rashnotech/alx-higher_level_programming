59966e21756d646ba39ceb61e2ce70bd1366521c
Rashnotech <rashnotech@gmail.com>
2023-06-28 10:42:36 +0000

    Fixed Error: Removed the and operator and declaration of private attribute in init

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -17,9 +17,10 @@ class MagicClass:
             a circle
     """
 
-    def __init__(self, radius):
-        self.__radius = 0
-        if type(radius) is not int and type(radius) is not float:
+    def __init__(self, radius=0):
+        if type(radius) is not int:
+            raise TypeError('radius must be a number')
+        elif type(radius) is not float:
             raise TypeError('radius must be a number')
         else:
             self.__radius = radius
