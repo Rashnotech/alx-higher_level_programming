a564dbfc717a83c26964425d96a7a74aaae8746c
Rashnotech <rashnotech@gmail.com>
2023-07-05 18:00:44 +0000

    Added error for positional argument

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 """ A function that prints a square with the character #
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than zero
 """
 
 
 def print_square(size):
+    """ A function that print a square
+        Args:
+            size: size of the square is supplied
+    """
     if not isinstance(size, int) or isinstance(size, float):
         raise TypeError('size must be an integer')
     elif size < 0:
