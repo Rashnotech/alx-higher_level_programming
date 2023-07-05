8fa12956eb2184f23855f22843639389aa5262ee
Rashnotech <rashnotech@gmail.com>
2023-07-05 16:44:35 +0000

    Added documentation to increase documentation

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,17 @@
     A function that prints first_name and last_name
     Exception:
         raised TypeError if first or last_name is not string
+    >>> say_my_name("John", "Smith")
+    My name is John Smith
+    >>> say_my_name("Walter", "White")
+    My name is Walter White
+    >>> say_my_name("Bob")
+    My name is Bob 
+    >>> try:
+    ...     say_my_name(12, "White")
+    ... except Exception as e:
+    ...     print(e)
+    first_name must be a string
 """
 
 
