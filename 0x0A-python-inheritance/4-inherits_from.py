2e9e413dd7d1ac91afa94eb8e4ca5e5bc957a485
Rashnotech <rashnotech@gmail.com>
2023-07-10 22:33:23 +0000

    an empty Geometry class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" A module that checks if an object is an instance of a class """
+
+
+def inherits_from(obj, a_class):
+    """ A function that checks if an object is an instance of a class
+        that inherits a specified class.
+        Return:
+            True otherwise false
+    """
+    if isinstance(obj, a_class) or issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
