430ea7f83c3d50e41921dcc0d9dfde519cb296ed
Rashnotech <rashnotech@gmail.com>
2023-07-11 01:10:58 +0000

    Check if an object is an instance of a clss thais inherited from the specified clas

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,7 +9,7 @@ def inherits_from(obj, a_class):
         Return:
             True otherwise false
     """
-    if isinstance(obj, a_class) or issubclass(type(obj), a_class):
+    if not type(obj) is a_class or not isinstance(obj, a_class):
         return True
     else:
         return False
