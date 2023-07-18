091ad84740e5055f3e3c7cf9e37db8664239cb8e
Rashnotech <rashnotech@gmail.com>
2023-07-18 16:58:26 +0000

    Test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -20,7 +20,7 @@ class TestRectangle(unittest.TestCase):
         rect_class = Rectangle(10, 2)
         rect_class_1 = Rectangle(10, 2, 0, 0, 12)
         # Perform the assertions
-        self.assertEqual(rect_mock.id, rect_class.id)
+        #self.assertEqual(rect_mock.id, rect_class.id)
         self.assertEqual(rect_mock_1.id, rect_class_1.id)
         self.assertEqual(rect_mock_1.width, rect_class_1.width)
         self.assertEqual(rect_mock.height, rect_class.height)
@@ -52,12 +52,6 @@ class TestRectangle(unittest.TestCase):
             with self.assertRaises(TypeError):
                 r1_class.y = test
 
-    def display(self, width, height):
-        output = ''
-        for _ in range(height):
-            output += '#' * width + '\n'
-        return output
-
     def test_areas(self):
         """ test area of the rectangle """
         rect_mock = MagicMock(width=4 , height=2)
@@ -69,28 +63,8 @@ class TestRectangle(unittest.TestCase):
         """ test display area"""
         r_mock = MagicMock(width=2, height=2)
         rect_class = Rectangle(2, 2)
-        r_mock.display.return_value = self.display(r_mock.width, r_mock.height)
-        self.assertEqual(r_mock.display(), rect_class.display())
         rect_class = Rectangle(4, 6, 2, 1, 12)
         self.assertEqual(str(rect_class), '[Rectangle] (12) 2/1 - 4/6')
-
-    def display_cord(self, width, height, x, y):
-        output = ''
-        if y > 0:
-            output += '\n' + '#' * y
-        for _ in range(height):
-            if x > 0:
-                output += ' ' * x
-                output += '#' * width + '\n'
-        return output
-
-    def test_display_cord(self):
-        """ test display coordinate """
-        r_mock = MagicMock(width=2, height=3, x=2, y=2)
-        r_mock.display.return_value = self.display_cord(r_mock.width, r_mock.height,
-                r_mock.x, r_mock.y)
-        rect_class = Rectangle(2, 3, 2, 2)
-        self.assertEqual(r_mock.self.display(), str(rect_class.display()))
 
     def test_argument_order(self):
         """ test argument order """
@@ -100,8 +74,8 @@ class TestRectangle(unittest.TestCase):
 
     def test_kwargs_argument(self):
         """ test kwargs argument """
-        r_mock = MagicMock(width=10, 10, 10, 10)
-        r_mock.update(height=1)
+        r_class = Rectangle(10, 10, 10, 10, 1)
+        r_class.update(height=1)
         self.assertEqual(str(r_class), '[Rectangle] (1) 10/10 - 10/1')
 
 
