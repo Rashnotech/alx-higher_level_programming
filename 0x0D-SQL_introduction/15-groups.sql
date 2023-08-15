e69c3e4038d1ef083af0b02376879ae5c5f46a1c
Rashnotech <rashnotech@gmail.com>
2023-08-15 22:12:44 +0000

    a script that lists the number of records with same score in the table

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,1 +1,4 @@
-SELECT score, COUNT(score) as 'number' from second_table GROUP BY score;
+-- a script that lists the number of records
+-- with the same score in the table
+SELECT score, COUNT(score) AS 'number' FROM second_table GROUP BY score;
+
