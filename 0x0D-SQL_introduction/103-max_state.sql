4f79714039ae39044ba11412d8f62217751cc8ac
Rashnotech <rashnotech@gmail.com>
2023-08-16 16:38:48 +0000

    Fixed: replace max(value) to custom property

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,4 @@
 -- a script that displays the max
 -- temperature of each state
-SELECT state, MAX(value) FROM temperatures GROUP BY state ORDER BY state ASC;
+SELECT state, MAX(value) AS 'max_temp' FROM temperatures GROUP BY state ORDER BY state ASC;
 
