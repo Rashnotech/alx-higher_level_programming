2b10d047469154fb8e9c5725b71cbad01b9b1a8d
Rashnotech <rashnotech@gmail.com>
2023-08-17 15:51:51 +0000

    Removed genres files

--- 0x0D-SQL_introduction/101-not_a_comedy.sql
+++
@@ -1,4 +0,0 @@
--- a script that lists all shows without the genre
-SELECT tv_shows.title
-FROM tv_gen
-ORDER BY tv_shows.title ASC;
--- 0x0D-SQL_introduction/103-rating_genres.sql
+++
@@ -1,4 +0,0 @@
--- a script that lists all genres by their rating
-SELECT tv_genres.name, AS 'rating'
-FROM
-ORDER BY rating DESC;
