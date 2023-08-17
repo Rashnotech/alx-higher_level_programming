6cba2fc8930936bb8fa74e96000bf136040fa0eb
Rashnotech <rashnotech@gmail.com>
2023-08-17 00:26:30 +0000

    a script that creates database and grant privilege to database

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- a script that create MySQL server
 -- first_user in Server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
