9c99ad21c0bf6d9343477219bcb9f3e89d1ad475
Rashnotech <rashnotech@gmail.com>
2023-08-17 00:45:57 +0000

    a script that creates a unique_id table

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- a script that creates a table with unique_id
+-- id with default value and must be unique
+CREATE TABLE IF NOT EXISTS unique_id(
+	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
+);
+
