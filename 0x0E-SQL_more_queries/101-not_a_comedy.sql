b8bfc3b50fce5c5b140af33e77add69e3e04d612
Rashnotech <rashnotech@gmail.com>
2023-08-18 00:04:12 +0000

    a script that list all show without the genre

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,8 +1,8 @@
 -- a script that lists all shows without the genre
 SELECT tv_shows.title
 FROM tv_shows
-WHERE tv_shows.id NOT IN (SELECT tv_genres.id
-	FROM tv_genres JOIN tv_show_genres
-	ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.id NOT IN (SELECT tv_show_genres.show_id
+	FROM tv_show_genres JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres.id
 	WHERE tv_genres.name = 'Comedy')
 ORDER BY tv_shows.title ASC;
