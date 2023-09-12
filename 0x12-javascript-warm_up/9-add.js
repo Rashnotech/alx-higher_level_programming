332eb7b5e6500d32074754a00c0805137673552c
Rashnotech <rashnotech@gmail.com>
2023-09-12 06:20:58 -0700

    a script that prints the addition of integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * a script that prints the addition of 2 integers
+ */
+const { argv } = require('process');
+const a = parseInt(argv[2]);
+const b = parseInt(argv[3]);
+function add (a, b) {
+  return a + b;
+}
+console.log(add(a, b));
