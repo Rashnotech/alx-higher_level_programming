c1df0a6099121aa69f925b1ac51593ec7d2802ef
Rashnotech <rashnotech@gmail.com>
2023-09-12 05:41:56 -0700

    a script that print 3 lines

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/**
+ * a script that print 3 lines
+ */
+const language = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < language.length; i++) console.log(language[i]);
