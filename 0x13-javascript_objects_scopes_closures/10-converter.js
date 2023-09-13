d97bdab8528b8a43c479e49dce585c9f8343513e
Rashnotech <rashnotech@gmail.com>
2023-09-12 18:36:47 -0700

    Fixed: revamped to avoid declaration

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return function (value) {
+  return function convertToBase (value) {
     if (value === 0) {
       return '0';
+    } else if (value < base) {
+      return value.toString(16);
+    } else {
+      return convertToBase(Math.floor(value / base)) + (value % base).toString(16);
     }
-
-    let result = '';
-    while (value !== 0) {
-      result = (value % base).toString(16) + result;
-      value = Math.floor(value / base);
-    }
-    return result;
   };
 };
