0e6e496be4d8320cd35b0c2c27b3de0f714eaaf3
Rashnotech <rashnotech@gmail.com>
2023-09-12 18:55:36 -0700

    Fixed: revamped to inherit from Square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const SquareX = require('./5-square');
+module.exports = class Square extends SquareX {
   charPrint (c) {
     if (!c) {
       c = 'X';
     }
-    for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        row += c;
-      }
-      console.log(row);
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 };
