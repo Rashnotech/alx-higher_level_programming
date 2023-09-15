e987abc25187100c453e3ff608403f5edc92c1e6
Rashnotech <rashnotech@gmail.com>
2023-09-15 13:31:49 +0000

    Using subqueries in MySQLdb connect

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" a script that lists all cities from the database """
+
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    my_user = sys.argv[1]
+    my_pass = sys.argv[2]
+    my_db = sys.argv[3]
+    search = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost', user=my_user, passwd=my_pass,
+                         db=my_db)
+    cur = db.cursor()
+    cur.execute('SELECT name FROM cities\
+            WHERE state_id = (SELECT id FROM states\
+            WHERE name = %s) ORDER BY id ASC', (search,))
+    cities = cur.fetchall()
+    for city, in cities:
+        print(city, end=", ")
+    print()
