bf401bd32f2faf38e6325bff106b43c008bd635f
Rashnotech <rashnotech@gmail.com>
2023-09-15 21:42:24 +0000

    a script that prints the first state object

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" a module that print the first State object from the database """
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    dbase = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        user, passwd, dbase), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by('id').first()
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
