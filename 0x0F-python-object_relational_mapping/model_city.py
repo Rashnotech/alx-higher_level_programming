6fb7a48847993ad5bbb1692973d39e88ddb609eb
Rashnotech <rashnotech@gmail.com>
2023-09-16 07:43:38 -0700

    Fixed: added semicolon to class

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -12,7 +12,7 @@ class City(Base):
     """ a City class that inherit from Base class"""
     __tablename__ = 'cities'
 
-    id = Column(Integer, autoincrement=True, unique=True
+    id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
