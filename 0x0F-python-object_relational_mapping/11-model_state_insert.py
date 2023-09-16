48b873b78f80eb1920aed9059b5bd0640d59780a
Rashnotech <rashnotech@gmail.com>
2023-09-16 00:09:01 +0000

    a script that adds to state objects

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,4 +8,16 @@ import sys
 
 
 if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    dbase = sys.argv[3]
 
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        user, passwd, dbase), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = State(name='Louisiana')
+    session.add(state)
+    session.commit()
+    print(state.id)
+    session.close()
