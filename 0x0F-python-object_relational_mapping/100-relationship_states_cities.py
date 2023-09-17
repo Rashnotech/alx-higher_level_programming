3ea66486d977145743917ef686aab72c4fca8219
Rashnotech <rashnotech@gmail.com>
2023-09-16 17:54:24 -0700

    Fixed: pycodestyle error

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -7,17 +7,15 @@ from relationship_state import Base, State
 import sys
 
 
-
 if __name__ == '__main__':
     user = sys.argv[1]
     passwd = sys.argv[2]
     dbase = sys.argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(user,passwd, dbase), pool_pre_ping=True)
+                           .format(user, passwd, dbase), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    state = State(name='California')
     city = City(name='San Francisco', state=State(name='California'))
     session.add(city)
     session.commit()
