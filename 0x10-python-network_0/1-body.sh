d2e182d207cd4bc9fa1d9463cf091e70da46abcb
Rashnotech <rashnotech@gmail.com>
2023-09-28 21:32:46 +0000

    a bash script that takes in a URL, sends a GET request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# a bash script that takes in a URL, sends a GET request to the URL, and
-# displays the body of the reponse
-if [[ $# -eq 1]]; then
-	curl --get $1
-else
-	echo "Usage: ./1-body.sh port"
-fi
+# a bash script that takes in a URL, sends a GET request to the URL, and displays the body of the reponse
+curl -sL -X GET $1
