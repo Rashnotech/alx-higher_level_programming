393ffd247b2e17734447d77dd29c78d24285e8ae
Rashnotech <rashnotech@gmail.com>
2023-09-28 17:21:42 +0000

    Fixed: removed conditionals

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # a bash script that takes in a URL, sends a request to that URL and 
 # displays the size of the body of the response
-if [[ $# -ne 1 ]]; then
-	echo "Usage: ./0-body_size.sh port"
-else
-	curl -s $1 | wc -c
-fi
+curl -s $1 | wc -c
