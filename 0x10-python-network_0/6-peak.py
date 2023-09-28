53f4ec878e28fe4a7181b419c18c39b418fbae59
Rashnotech <rashnotech@gmail.com>
2023-09-28 23:29:25 +0000

    a function that finds a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a module that find a peak"""
+
+def find_peak(arr):
+    """
+        a function that finds a peak in a list of unsorted integers
+        Args:
+            arr: an array of unsorted integers
+        Returns:
+            function returns peak in the list of integers
+    """
+    size = len(arr)
+    if size != 0:
+        for i in range(size):
+            x = i + 1;
+            if x != size-1:
+                peak = arr[i] + arr[x]
+                if peak == arr[x + 1]:
+                    return arr[x + 1]
+            else:
+                return max(arr)
+    else:
+        return None
