dfa2a4568f1f030a3ce5879bc6a2abda6af9676d
Rashnotech <rashnotech@gmail.com>
2023-09-29 22:28:04 +0000

    python script

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,5 +6,5 @@ import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     content = response.read()
     print('Body response:')
-    print('\t- type:{}\n\t- content: {}\n\t- utf8 content: {}'.format(
+    print('\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'.format(
           type(content), content, content, content.decode('utf-8')))
