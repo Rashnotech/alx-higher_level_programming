8fd788b083b5fa992058f8d5d457c0aa23d0298c
Rashnotech <rashnotech@gmail.com>
2023-09-30 19:54:53 +0000

    Fixed: import scope

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,8 +4,9 @@ import urllib.request
 from sys import argv
 
 
-url = argv[1]
-email = argv[2]
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-with urllib.request.urlopen(url, data=data) as response:
-    body = response.read().decode('utf-8')
+if __name__ == '__main__':
+    url = argv[1]
+    email = argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    with urllib.request.urlopen(url, data=data) as response:
+        body = response.read().decode('utf-8')
