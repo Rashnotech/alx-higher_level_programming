72eb1e8cbfce14c7fd8ca1ffb25e7289d9513812
Rashnotech <rashnotech@gmail.com>
2023-09-30 19:55:53 +0000

    Fixed: print server response

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,3 +10,4 @@ if __name__ == '__main__':
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     with urllib.request.urlopen(url, data=data) as response:
         body = response.read().decode('utf-8')
+        print(body)
