e5cd8592b438ef72cf3f0919a7459cd3e9e70cbb
Rashnotech <rashnotech@gmail.com>
2023-10-24 20:00:35 +0000

    Fixed: code style

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,9 @@
 /**
  * a script that display the status code of a GET request
  */
-const fs = require('fs');
 const { argv } = require('process');
 const request = require('request');
 
-request.get(argv[2]).on('response', function(response) {
+request.get(argv[2]).on('response', function (response) {
   console.log('code:', response.statusCode);
 });
