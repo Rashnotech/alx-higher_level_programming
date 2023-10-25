9f6e0cbfc9cdfb2289dd0e0f476a4f61ff41266f
Rashnotech <rashnotech@gmail.com>
2023-10-25 00:00:24 +0000

    Fixed: starwars count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,8 +1,22 @@
 #!/usr/bin/node
 /**
- * a script that print the number of movies
+ * a script that prints the title of a star wars
  */
 const { argv } = require('process');
-const require = require('require');
+const request = require('request');
 
+let count = 0;
 
+request(argv[2], function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    for (const movie of JSON.parse(body).results) {
+      const characters = movie.characters;
+      for (const list of characters) {
+        if (list.endsWith('/18/')) count++;
+      }
+    }
+  } else {
+    console.error('Error: Unable to fetch movie data');
+  }
+  console.log(count);
+});
